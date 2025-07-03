7f725251225682b57451b27e089de626cae377b1
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-07-03 11:11:04 -0700

    fast-forward to 1.2.0 logic from fabric
    * Kill if team knocked out. Kills you if, after one of your Pokémon faints in or out of battle, you die.
    * Kill if lost. Kills you if you lose a Pokémon battle.
    * Kill if fled. Kills you if you flee a battle. Comes with its own message!

--- src/main/kotlin/us/timinc/mc/cobblemon/whiteout/config/WhiteoutConfig.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/whiteout/config/WhiteoutConfig.kt
@@ -3,4 +3,7 @@ package us.timinc.mc.cobblemon.whiteout.config
 class WhiteoutConfig {
     val healTeam: Boolean = false
     val showMessage: Boolean = true
+    val killIfTeamKnockedOut: Boolean = true
+    val killIfLost: Boolean = false
+    val killIfFled: Boolean = false
 }
